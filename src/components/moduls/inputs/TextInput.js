7532afd644df2166302fa75215107ebc446743f2
sarah <sschmidt0@uoc.edu>
2021-10-14 09:00:54 +0200

    added close functionality to click outside text input or list

--- src/components/moduls/inputs/TextInput.js
+++ src/components/moduls/inputs/TextInput.js
@@ -1,38 +1,55 @@
+import { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { PlaceListBox } from '../../PlaceListBox';
 import { StyledTextInput } from './TextInput.styles';
 import Icon from '@mdi/react';
 import { mdiAirplane } from '@mdi/js';
 
-export const TextInput = ({ label, value, setInputValue, placeList, isFocused, setIsFocused, setValue, error }) => (
-  <StyledTextInput>
-    <label htmlFor={ label }>{ label }</label>
-    <div>
-      <Icon path={ mdiAirplane }
-        horizontal
-        vertical
-        rotate={ 180 }
-      />
-      <input
-        type="text"
-        name={ label }
-        value={ value }
-        onChange={ (e) => setInputValue(e.target.value) }
-        onFocus={ () => setIsFocused(true) }
-        // onBlur={ () => setIsFocused(false) }
-      />
-    </div>
-    { error !== '' && <span className="error-span">{ error }</span> }
-    {
-      placeList !== 'undefined' && isFocused &&
-      <PlaceListBox
-        list={ placeList }
-        setValue={ setValue }
-        setIsFocused={ setIsFocused }
-      />
+export const TextInput = ({ label, value, setInputValue, placeList, isFocused, setIsFocused, setValue, error }) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      if (isFocused && inputRef.current && !inputRef.current.contains(e.target)) setIsFocused(false);
+    };
+
+    document.addEventListener("mousedown", checkIfClickedOutside);
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside);
     }
-  </StyledTextInput>
-);
+    //eslint-disable-next-line
+  }, [isFocused]);
+
+  return (
+    <StyledTextInput ref={ inputRef }>
+      <label htmlFor={ label }>{ label }</label>
+      <div>
+        <Icon path={ mdiAirplane }
+          horizontal
+          vertical
+          rotate={ 180 }
+        />
+        <input
+          type="text"
+          name={ label }
+          value={ value }
+          onChange={ (e) => setInputValue(e.target.value) }
+          onFocus={ () => setIsFocused(true) }
+        />
+      </div>
+      { error !== '' && <span className="error-span">{ error }</span> }
+      {
+        placeList !== 'undefined' && isFocused &&
+        <PlaceListBox
+          list={ placeList }
+          setValue={ setValue }
+          setIsFocused={ setIsFocused }
+        />
+      }
+    </StyledTextInput>
+  );
+};
 
 TextInput.propTypes = {
   label: PropTypes.string,
